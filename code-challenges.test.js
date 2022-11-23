a6c78aa3a9da769262c745fd9bc486ee03b2ef50
Shadow090509 <jamoso090509@gmail.com>
2022-11-23 11:19:23 -0800

    pushed new

---
+++ code-challenges.test.js
@@ -0,0 +1,97 @@
+// ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
+
+// Please read all questions thoroughly
+// Pseudo coding is REQUIRED
+// If you get stuck, please leave comments to help us understand your thought process
+
+// Use test driven development to complete the following questions
+// Add appropriate dependencies: $ yarn add jest
+
+// Reminder: The test will call your function
+// Run the file with the following command: $ yarn jest
+
+// --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
+//I shall call it chopShuffle. the name is self explanitory.
+
+// a) Create a test with an expect statement using the variable provided.
+// HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// After checking out the link i decided to creat the testing perameters.
+
+// describe("chopShuffle", () => {
+//   //moved the variable into the testing perameters.
+//   const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+//   it("removes the first item from the array and shuffles the remaining content", () => {
+//     // After research, i noticed .toContain will check to see if the array contains any values i wish to expect in no particular order. i thought this is a good idea since the order is going to be randomized.
+//     expect(chopShuffle(colors1)).toContain("yellow", "blue", "pink", "green");
+//   });
+// });
+//test failed successfully
+// Now i create the function.
+// const chopShuffle = (array) => {
+//   array.slice(0);
+//   return array.sort(math.floor(math.random() * array.length));
+// };
+
+// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+// const colors2 = [
+//   "chartreuse",
+//   "indigo",
+//   "periwinkle",
+//   "ochre",
+//   "aquamarine",
+//   "saffron",
+// ];
+// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+
+// b) Create the function that makes the test pass.
+
+// --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
+// it looks like im just making a function that subtracts the two Votes and outputs the result
+
+// a) Create a test with expect statements for each of the variables provided.
+// describe("tally", () => {
+//   const votes1 = { upVotes: 13, downVotes: 2 };
+//   const votes2 = { upVotes: 2, downVotes: 33 };
+//   it("returns the end tally", () => {
+//     expect(tally(votes1)).toEqual(11);
+//     expect(tally(votes2)).toEqual(-31);
+//   });
+// });
+
+// Expected output: 11
+
+// Expected output: -31
+// I'm feeling a bit lost with this one. i can't wait to see how to solve this one
+// const tally = (array) => {
+//   return { upVotes } - { downVotes };
+// };
+
+// b) Create the function that makes the test pass.
+
+// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+// describe("newString", () => {
+const dataArray1 = ["array", "object", "number", "string", "Boolean"];
+const dataArray2 = ["string", "null", "Boolean", "string", "undefined"];
+//   it("returns one array with no duplicate values", () => {
+//     expect(newString(dataArray1, dataArray2)).toEqual([
+//       "array",
+//       "object",
+//       "number",
+//       "string",
+//       "Boolean",
+//       "null",
+//       "undefined",
+//     ]);
+//   });
+// });
+// this one is also stumping me.
+const newString = () => {
+  return dataArray1.splice(dataArray2).concat(dataArray2.splice(dataArray1));
+};
+console.log(newString());
+// I know it's hard coded. but i was trying to figure out how to combine the two strings without having duplicates..
+// a) Create a test with an expect statement using the variables provided.
+
+// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
+// b) Create the function that makes the test pass.
---
+++ interview-questions.md
@@ -0,0 +1,49 @@
+# ASSESSMENT 4: Interview Practice Questions
+
+Answer the following questions.
+
+First, without external resources. Challenge yourself to answer from memory.
+
+Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
+
+1. What is object-oriented programming? How is it different than functional programming?
+
+Your answer: From my understanding, that means that most(if not all things are objects) I think this would be slightly easier to mannage and just apply classes to the objects in stead of having to create the objects as well.
+
+Researched answer: OOP was made to make it easier to work with large software systems. a way to create containers for data that can be changed and manipulated without messing up the rest of the program.
+
+2. What is the difference between a Float and an Integer in Ruby?
+
+Your answer: an integer is a whole number. and float is a decimal number.
+
+Researched answer: integer is a number without a decimal point. A float is a floating point number and has a decimal place.
+
+3. Ruby has an implicit return. What does that mean?
+
+Your answer: if there is no return created, the last command will be automatically returned. like a default
+
+Researched answer: if there is no return "explicitly" prompted within a method, the value of the last executed instruction is returned.
+
+4. What is a block in Ruby?
+
+Your answer: like an anonymous function.
+
+Researched answer: some typical blocks are do and end. they are passed to a method to determine an outcome.
+
+1. How do you extract a value in a Ruby hash?
+
+Your answer: by calling upon the key/value pair.
+
+Researched answer: calling the hash followed by a key name within brackets grabs the value associated with the key.
+
+## Looking Ahead: Terms for Next Week
+
+1. Class Inheritance:
+
+2. RSpec:
+
+3. CRUD:
+
+4. Test-driven development:
+
+5. HTTP:
